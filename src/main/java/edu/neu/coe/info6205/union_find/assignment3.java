7a10d3384bec7c11e6a245a6d4b1e658a2bb4c32
Namrata2108 <42319987+Namrata2108@users.noreply.github.com>
2021-10-07 16:32:27 -0400

    Assignment 1

---
+++ src/main/java/edu/neu/coe/info6205/union_find/assignment3.java
@@ -0,0 +1,30 @@
+package edu.neu.coe.info6205.union_find;
+import java.util.Scanner;
+
+public class assignment3
+{
+    public static void main(String[] args)
+    {
+        int p, q;
+        Scanner s = new Scanner(System.in);
+        long N = s.nextLong();
+        UF_HWQUPC uf = new UF_HWQUPC((int) N);
+        int count=0;
+        int temp= uf.components();
+
+        while(temp!=1)
+        {
+            p = (int) (Math.random() * (N - 1));
+            q = (int) (Math.random() * (N - 1));
+
+            if (!uf.connected(p, q))
+            {
+                uf.union(p, q);
+            }
+            count++;
+            temp= uf.components();
+        }
+        System.out.println("Number of connections: "+ count);
+    }
+
+}
